d2d6b0d7be416d9380ca2b6cd0e880ed87a680de
Maxim Shkividorov <32208852+Tomeister@users.noreply.github.com>
2018-04-09 15:25:18 +0300

    Update ConsumerAndProducer.java

--- Java/Third lab/ConsumerAndProducer.java
+++ Java/Third lab/ConsumerAndProducer.java
@@ -5,8 +5,8 @@ public class ConsumerAndProducer {
         Stock stock = new Stock();
         Producer producer = new Producer(stock);
         Consumer consumer = new Consumer(stock);
-          new Thread(producer).start();
-          new Thread(consumer).start();
+            new Thread(producer).start();
+            new Thread(consumer).start();
     }
 
 }
@@ -44,32 +44,24 @@ class Consumer implements Runnable {
 }
 
 class Stock {
-    private int product=0;
-
+    
     public synchronized void get(int random) {
-        while (product<1) {
+        System.out.println("Покупатель купил товар " + random );
+        notify();
             try {
                 wait();
             }
             catch (InterruptedException e) {
-            }
-        }
-        product--;
-        System.out.println("Покупатель купил товар " + random );
-        notify();
+           }
     }
 
     public synchronized void put(int random) {
-        while (product>=1) {
+        System.out.println("Производитель добавил товар " + random);
+        notify();
             try {
                 wait();
             }
             catch (InterruptedException e) {
             }
-        }
-        product++;
-        System.out.println("Производитель добавил товар " + random);
-        System.out.println("Товаров на складе: " + product);
-        notify();
     }
 }

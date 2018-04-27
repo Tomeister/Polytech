fd26a6b871dac509fe0c62d35399ae5e2453cfb3
Maxim Shkividorov <32208852+Tomeister@users.noreply.github.com>
2018-04-27 19:00:00 +0300

    Update MainActivity.java

--- Java/SeventhLab/java/com/example/max/myroulette/MainActivity.java
+++ Java/SeventhLab/java/com/example/max/myroulette/MainActivity.java
@@ -28,23 +28,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.button2); //чтобы обратится к константе нужно написать R.id.myText
-        textView = (TextView) findViewById(R.id.textView); //findVievByID возвращает по ID - View
+        button = (Button) findViewById(R.id.button2); 
+        textView = (TextView) findViewById(R.id.textView); 
         iv_ruletka = (ImageView) findViewById(R.id.iv_ruletka);
 
         r = new Random();
 
-        button.setOnClickListener(new View.OnClickListener() {  //setOnClickListener регирование кнопки на нажатие
+        button.setOnClickListener(new View.OnClickListener() {  
             @Override
             public void onClick(View v) {
                 degree_old = degree % 360;
                 degree = r.nextInt(3600) + 720;
                 RotateAnimation rotate = new RotateAnimation(degree_old,degree,
                         RotateAnimation.RELATIVE_TO_SELF, 0.5f, RotateAnimation.RELATIVE_TO_SELF, 0.5f);
-                rotate.setDuration(3600); //Как долго эта анимация должна продолжаться.
-                rotate.setFillAfter(true); //Если fillAfter имеет значение true, преобразование, которое выполняет эта анимация, сохранится, когда оно будет завершено.
-                rotate.setInterpolator(new DecelerateInterpolator()); //Устанавливает кривую ускорения для этой анимации.
-                rotate.setAnimationListener(new Animation.AnimationListener() { //Привязывает анимацию к этой анимации.
+                rotate.setDuration(3600); 
+                rotate.setFillAfter(true); 
+                rotate.setInterpolator(new DecelerateInterpolator()); 
+                rotate.setAnimationListener(new Animation.AnimationListener() { 
                     @Override
                     public void onAnimationStart(Animation animation) {
 

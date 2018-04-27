b50e711a88278b5b570a6784d87a33e2ccef6413
Maxim Shkividorov <32208852+Tomeister@users.noreply.github.com>
2018-04-27 18:58:30 +0300

    Update Last version consumer.java

--- Java/Third lab/Last version consumer.java
+++ Java/Third lab/Last version consumer.java
@@ -1,1 +1,66 @@
+import java.util.Random;
 
+public class ConsumerAndProducer {
+    public static void main(String[] args) {
+        Stock stock = new Stock();
+        Producer producer = new Producer(stock);
+        Consumer consumer = new Consumer(stock);
+        new Thread(producer).start();
+        new Thread(consumer).start();
+    }
+}
+
+
+class Producer implements Runnable {
+    Stock stock;
+    Producer(Stock stock){
+        this.stock=stock;
+    }
+
+    Random random = new Random(47);
+
+    @Override
+    public void run(){
+        for (int i = 1; i < 100; i++) {
+
+            stock.put(random.nextInt(100));
+        }
+    }
+}
+
+class Consumer implements Runnable {
+    Stock stock;
+    Consumer(Stock stock){
+        this.stock=stock;
+    }
+    Random random = new Random(47);
+    @Override
+    public void run(){
+        for (int i = 1; i < 100; i++) {
+            stock.get(random.nextInt(100));
+        }
+    }
+}
+
+class Stock {
+
+    public synchronized void get(int random) {
+        System.out.println("Покупатель купил товар " + random );
+        notify();
+            try {
+                wait();
+            }
+            catch (InterruptedException e) {
+           }
+    }
+
+    public synchronized void put(int random) {
+        System.out.println("Производитель добавил товар " + random);
+        notify();
+            try {
+                wait();
+            }
+            catch (InterruptedException e) {
+            }
+    }
+}
